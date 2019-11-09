051f110114e3075a0991e0006a7db59e4de1f686
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 12:01:10 -0800

    major packages installed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hello");
-
-const x = 5
-console.log(x + 2);
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("listening at port 3000"));
+app.use(express.static("public"));
