0ec9b39cb1f8ed3ca3bd24a1a6ef59e02efd408f
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 19:08:43 -0800

    a

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
-app.get("/test", (request, response) => {
+app.get("/summoner/:id", (request, response) => {
   //console.log(request.body);
-  console.log("OH YES IT WORKS!!!!!");
-  response.send("PlayerPage.html");
+  console.log(request.params.id);
+  response.send(request.params.id);
   /*response.json({
     status: "success"
   });*/
