d43319833ff81d5e2dfab9b7362d021196b0839a
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 17:46:41 -0800

    d

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const API_KEY = "RGAPI-40b2689e-8392-4bb1-81bd-8856aa825e1c";
-
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
-//app.use(express.json());
+app.use(express.json());
 //{ limit: "1mb" }
 app.post("/api", (request, response) => {
-  console.log(request);
+  console.log(request.body);
 });
