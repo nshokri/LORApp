083f02ba240818d618e227c030c1f02a56ab8055
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 16:19:39 -0800

    added post request to server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const API_KEY = "RGAPI-40b2689e-8392-4bb1-81bd-8856aa825e1c";
+
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
 
-const API_KEY = "RGAPI-40b2689e-8392-4bb1-81bd-8856aa825e1c";
+app.post("/api", (request, response) => {
+  console.log(request);
+
+});
