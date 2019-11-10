4c5037e8bf31e5e47e2932d0d59b523a6de105f6
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 21:03:26 -0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
 var user;
 
 app.listen(3000, () => console.log("listening at port 3000"));
@@ -13,13 +14,11 @@ app.get("/summoner/:id", (request, response) => {
   user = request.params.id;
 
   response.sendFile(path.join(__dirname + "/summoner.html"));
-  response.json();
 });
 
 app.post("/loadInfo", (request, response) => {
 
   response.json({
     para1: user,
-    status: "success"
   });
 });
