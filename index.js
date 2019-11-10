c306bec1359136f42460bdbc47d24a103986f13a
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 17:19:30 -0800

    server parses json and only takes useful stuff

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/api", (request, response) => {
-  console.log(request);
+  console.log(request.body);
 
 });
