d9faa8e3539f270abab46628d0bcaee561ed706e
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 19:47:52 -0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+var user;
 
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
@@ -9,6 +10,11 @@ app.use(express.json({ limit: "1mb" }));
 app.get("/summoner/:id", (request, response) => {
   //console.log(request.body);
   console.log(request.params.id);
+  user = request.params.id;
+
+  //Create summoner site before send
+  summoner.getElementById("Head").innerHTML = "TESTING";
+
   response.sendFile(path.join(__dirname + "/summoner.html"));
   /*response.json({
     status: "success"
