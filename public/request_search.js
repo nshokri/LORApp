054e5d56952e6c469ce2e74e672dcfe6f9c56662
FurtleTurtle <57571990+FurtleTurtle@users.noreply.github.com>
2019-11-09 17:04:41 -0800

    fixed submition page, uses form now

--- public/request_search.js
+++
@@ -1,9 +0,0 @@
-const data = { player_name };
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-};
-fetch('/api', options);
