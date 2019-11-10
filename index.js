e250506561463f3ea9f9c3f517981e11296fef64
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-11-09 17:28:18 -0800

    yuhh

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
-app.use(express.json({ limit: "1mb" }));
-
+app.use(express.json());
+//{ limit: "1mb" }
 app.get("/api", (request, response) => {
   console.log(request.body);
+  response.json({
+    status: "success"
 
+  });
 });
