c700d9ce38024f4876bc1b4fffd8050fcdc0100d
s-shokrin <31524605+s-shokrin@users.noreply.github.com>
2019-12-06 16:46:49 -0800

    Adding comments as a to-do-list for later

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var user;
 app.listen(3000, () => console.log("listening at port 3000"));
 app.use(express.static("public"));
 app.use(express.json());
-//{ limit: "1mb" }
+//{ limit: "1mb" } 
 
 app.get("/summoner/name=:id", (request, response) => {
   console.log(request.params.id);
